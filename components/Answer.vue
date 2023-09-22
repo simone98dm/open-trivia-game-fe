f86275debe69d675e84e34239e45d74e5f888ba3
simone98dm <simone.dalmas@outlook.it>
2023-09-22 09:19:41 +0200

    improve styles for answers

--- components/Answer.vue
+++ components/Answer.vue
@@ -12,11 +12,12 @@ defineProps<AnswerProps>();
 <template>
   <span
     :class="[
+      'answer',
       {
         correct: showAnswer && answer.correct,
         incorrect: showAnswer && !answer.correct,
       },
-      'answer',
+      { [`${answer.id}`]: !showAnswer },
     ]"
   >
     {{ answer.value }}
@@ -26,14 +27,32 @@ defineProps<AnswerProps>();
 <style scoped>
 .answer {
   margin: 8px;
-  padding: 4px 16px;
+  padding: 16px 32px;
   border-radius: 24px;
-  font-size: 32px;
+  font-size: 18px;
 }
 .correct {
   color: green;
+  background-color: #eeeeee;
 }
 .incorrect {
   color: red;
+  background-color: #eeeeee;
+}
+.a-0 {
+  background-color: #0000ff;
+  color: white;
+}
+.a-1 {
+  background-color: #ff0000;
+  color: white;
+}
+.a-2 {
+  background-color: #008000;
+  color: white;
+}
+.a-3 {
+  background-color: #ffa500;
+  color: white;
 }
 </style>
